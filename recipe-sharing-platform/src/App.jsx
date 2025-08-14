87ed937acef85fe9642bb8c51ef522ded9b70f51
Christy Ekpiken <christyekpixs99@gmail.com>
2025-08-14 14:33:27 +0100

    Responsive design

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,14 +1,11 @@
+import HomePage from "./components/HomePage";
+
 function App() {
   return (
-    <div className="text-center p-10">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Tailwind is working 🎉
-      </h1>
-      <p className="mt-4 text-gray-600">
-        Your Recipe Sharing Platform setup is ready.
-      </p>
+    <div className="bg-gray-100 min-h-screen">
+      <HomePage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
