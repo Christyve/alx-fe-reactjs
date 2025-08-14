d324cb492a171344f717dd1ac28e20f6dbaa7cb4
Christy Ekpiken <christyekpixs99@gmail.com>
2025-08-14 14:23:19 +0100

    commit

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './index.html',                // Root HTML file (Vite)
     './src/**/*.{js,jsx,ts,tsx}',  // All React files
+    './public/index.html',
   ],
   theme: {
     extend: {},
