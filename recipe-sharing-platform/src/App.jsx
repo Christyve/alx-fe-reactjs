16174716853a61d92620cf1178c43e97126c9898
Christy Ekpiken <christyekpixs99@gmail.com>
2025-08-14 13:12:11 +0100

    Recipe Sharing Platform

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Tailwind is working!
+      </h1>
+      <p className="mt-4 text-gray-600">
+        You’ve bypassed the npx issue.
+      </p>
+    </div>
+  )
+}
+
+export default App
