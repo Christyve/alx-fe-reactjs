d3b080b4a865af7f63ffeaf0719107a4f0723a86
Christy Ekpiken <christyekpixs99@gmail.com>
2025-08-25 15:12:28 +0100

    commit

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,32 +1,10 @@
-// src/App.jsx
-import React, { useState } from "react";
+import React from "react";
+import TodoList from "./components/TodoList";  // 👈 Import TodoList
 
 function App() {
-  const [todos, setTodos] = useState(["Learn React", "Build a Todo App"]);
-  const [newTodo, setNewTodo] = useState("");
-
-  const addTodo = () => {
-    if (newTodo.trim()) {
-      setTodos([...todos, newTodo]);
-      setNewTodo("");
-    }
-  };
-
   return (
     <div>
-      <h1>Todo List</h1>
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
-      <input
-        data-testid="todo-input"
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add</button>
+      <TodoList />  {/* 👈 Render TodoList */}
     </div>
   );
 }
